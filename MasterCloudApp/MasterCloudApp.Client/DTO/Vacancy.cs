4197261942574d0d57e604631a72fe4b6589518e
Артем Борисович Козиев <akoziev@btlab.ru>
2024-11-15 16:03:35 +0300

    Доработан ApiService, добавлен DTO для вакансий

---
+++ MasterCloudApp/MasterCloudApp.Client/DTO/Vacancy.cs
@@ -0,0 +1,12 @@
+﻿namespace MasterCloudApp.Client.DTO
+{
+    public class Vacancy
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool AccreditedItEmployer { get; set; } = false;
+        public int SalaryFrom { get; set; } = 0;
+        public string SalaryCurrency { get; set; } = string.Empty;
+        public string AreaName { get; set; } = string.Empty;
+        public string ExperienceName { get; set; } = string.Empty;
+    }
+}
--- MasterCloudApp/MasterCloudApp.Client/Services/ApiService.cs
+++ MasterCloudApp/MasterCloudApp.Client/Services/ApiService.cs
@@ -3,6 +3,7 @@ using Amazon.S3;
 using Newtonsoft.Json.Linq;
 using System.Reflection;
 using Newtonsoft.Json;
+using MasterCloudApp.Client.DTO;
 
 namespace MasterCloudApp.Client.Services
 {
@@ -12,7 +13,7 @@ namespace MasterCloudApp.Client.Services
 
         public int VacanciesPerPage { get; set; }
 
-        public async Task<string> Start(int pageCount = 2, int vacanciesPerPage = 10)
+        public async Task<string> Start(int pageCount = 2, int vacanciesPerPage = 100)
         {
             PageCount = pageCount;
             VacanciesPerPage = vacanciesPerPage;
@@ -23,12 +24,55 @@ namespace MasterCloudApp.Client.Services
 
         private async Task GetVacanciesJson()
         {
+            HashSet<Vacancy> vacancies = new HashSet<Vacancy>();
             for (int page = 0; page < PageCount; page++)
             {
-                string json = await GetPageAsync(page, VacanciesPerPage);
-                File.WriteAllText(@$"D:\VisualStudioProjects\MasterCloudApp\MasterCloudApp\MasterCloudApp.Client\{page}.json", json);
+                JArray jarray = await GetPageAsync(page, VacanciesPerPage);
+                foreach (JObject jobject in jarray)
+                {
+                    Vacancy vacancy = new Vacancy();
+                    JToken? nameT = jobject.SelectToken("name");
+                    JToken? empT = jobject.SelectToken("employer");
+                    JToken? salaryT = jobject.SelectToken("salary");
+                    JToken? areaT = jobject.SelectToken("area");
+                    JToken? experienceT = jobject.SelectToken("experience");
+
+                    vacancy.Name = nameT != null ? nameT.Value<string>() : string.Empty;
+
+                    if (empT != null && empT.HasValues)
+                    {
+                        bool? AccreditedItEmployer = empT.Value<bool?>("accredited_it_employer");
+                        vacancy.AccreditedItEmployer = AccreditedItEmployer.HasValue ? AccreditedItEmployer.Value : false;
+                    }
+
+                    if (salaryT != null && salaryT.HasValues)
+                    {
+                        string currency = salaryT.Value<string>("currency");
+                        int? from = salaryT.Value<int?>("from");
+                        vacancy.SalaryCurrency = currency ?? string.Empty;
+                        vacancy.SalaryFrom = from.HasValue ? from.Value : 0;
+                    }
+
+                    if (areaT != null && salaryT.HasValues)
+                    {
+                        string name = areaT.Value<string>("name");
+                        vacancy.AreaName = name ?? string.Empty;
+                    }
+
+                    if (experienceT != null && salaryT.HasValues)
+                    {
+                        string name = experienceT.Value<string>("name");
+                        vacancy.ExperienceName = name ?? string.Empty;
+                    }
+
+                    vacancies.Add(vacancy);
+                }
+                //File.WriteAllText(@$"D:\VisualStudioProjects\MasterCloudApp\MasterCloudApp\MasterCloudApp.Client\{page}.json", json);
                 await Task.Delay(250);
             }
+
+            string json = JsonConvert.SerializeObject(vacancies, Formatting.Indented);
+            Console.WriteLine(json);
         }
 
         private async Task<string> SendVacanciesJson()
@@ -54,11 +98,12 @@ namespace MasterCloudApp.Client.Services
         }
 
         // Метод для получения страницы вакансий
-        private static async Task<string> GetPageAsync(int page, int perPage)
+        private static async Task<JArray> GetPageAsync(int page, int perPage)
         {
             using var client = new HttpClient();
-            var url = $"https://api.hh.ru/vacancies?text=NAME:Программист&area=53&page={page}&per_page={perPage}";
-            return await client.GetStringAsync(url);
+            var url = $"https://api.hh.ru/vacancies?text=NAME:Программист&page={page}&per_page={perPage}";
+            var response = await client.GetStringAsync(url);
+            return JObject.Parse(response)["items"] as JArray;
         }
     }
 }
